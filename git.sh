e9e8ec3bd6d4d03d1e0bbb6964814389d87956ac
githubWangHC <whc15@outlook.com>
2017-07-07 17:58:21 +0800

    add file in ''

--- git.sh
+++ git.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #this progran is used to sync the direction ~/github/ wiht GitHub
 git add ~/githubWangHC/
-git commit -m "add "
+date_now = $(date +%Y%m%d%H%M%S)
+git commit -m "add file in '$date_now'"
 git push origin master
