3b07d139fbec81c33be577b3dc6e75f32aafb60f
githubWangHC <whc15@outlook.com>
2017-11-22 22:37:23 +0800

    add file in

---
+++ C_language/usb.c
@@ -0,0 +1,40 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <string.h>  
+#include <fcntl.h>  
+#include <sys/socket.h>  
+#include <linux/netlink.h>  
+#define UEVENT_BUFFER_SIZE 2048  
+int main(void)  
+{  
+    struct sockaddr_nl client;  
+    struct timeval tv;  
+    int ntSocket, rcvlen, ret;  
+    fd_set fds;  
+    int buffersize = 1024;  
+    ntSocket = socket(AF_NETLINK, SOCK_RAW, NETLINK_KOBJECT_UEVENT);  
+    memset(&client, 0, sizeof(client));  
+    client.nl_family = AF_NETLINK;  
+    client.nl_pid = getpid();  
+    client.nl_groups = 1; /* receive broadcast message*/  
+    setsockopt(ntSocket, SOL_SOCKET, SO_RCVBUF, &buffersize, sizeof(buffersize));  
+    bind(ntSocket, (struct sockaddr*)&client, sizeof(client));  
+    while (1) {  
+        char buf[UEVENT_BUFFER_SIZE] = { 0 };  
+        FD_ZERO(&fds);  
+        FD_SET(ntSocket, &fds);  
+        tv.tv_sec = 0;  
+        tv.tv_usec = 100 * 1000;  
+        ret = select(ntSocket + 1, &fds, NULL, NULL, &tv);  
+        if(!(ret > 0 && FD_ISSET(ntSocket, &fds)))  
+            continue;  
+        /* receive data */  
+        rcvlen = recv(ntSocket, &buf, sizeof(buf), 0);  
+        if (rcvlen > 0) {  
+            printf("%s\n", buf);  
+            // You can do something here to make the program more perfect!!!  
+        }  
+    }  
+    close(ntSocket);  
+    return 0;  
+}  
